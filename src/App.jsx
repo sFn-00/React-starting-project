08d941783a2552fc02fec029bede407bfc931a38
Szymon Szafoni <szymon.szafoni@gca.pass-consulting.com>
2024-10-30 14:15:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
